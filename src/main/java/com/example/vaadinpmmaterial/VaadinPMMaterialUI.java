fcdcc17286d59429666118b192e6e77d3568e722
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2019-03-29 11:11:03 +0300

    Small fix

--- src/main/java/com/example/vaadinpmmaterial/VaadinPMMaterialUI.java
+++ src/main/java/com/example/vaadinpmmaterial/VaadinPMMaterialUI.java
@@ -1,11 +1,9 @@
 package com.example.vaadinpmmaterial;
 
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.material.Material;
 
-@HtmlImport("frontend://styles/vaadinpmmaterial-theme.html")
 @Theme(value = Material.class, variant = "dark")
 public class VaadinPMMaterialUI extends UI {
 
